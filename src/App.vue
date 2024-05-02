bf9c72d62e6c9997ba1d460b651d756ad9c2f319
hasinduK <it22234452@my.sliit.lk>
2024-05-02 10:15:25 +0530

    Update Header and Footer
    Icons not displaying. need to fix it.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,40 @@
 <template>
-<PageHeader/>
-<!-- <UserLogin/> -->
-<HomePage/>
-<PageFooter/>
-
+  <PageHeader />
+  <!-- <UserLogin/> -->
+  <HomePage style="margin-top: 2%;"/>
+  <PageFooter />
 </template>
 
 <script>
-import PageHeader from './components/PageHeader.vue'
+import PageHeader from "./components/PageHeader.vue";
 // import UserLogin from './components/UserLogin.vue'
-import PageFooter from './components/PageFooter.vue'
-import HomePage from './components/HomePage.vue';
+import PageFooter from "./components/PageFooter.vue";
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     PageHeader,
     //UserLogin,
     PageFooter,
-    HomePage
-  }
-}
+    HomePage,
+  },
+};
 </script>
 
 <style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 0px; */
 }
 </style>
--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,43 +1,66 @@
 <template>
-    <footer class="footer-container">
-      <div class="footer-column">
-        <h3>Contact</h3>
-        <p>Phone: +123456789</p>
-        <p>Email: example@example.com</p>
+  <footer class="footer-container">
+    <!-- <div class="footer-column">
+      
+    </div> -->
+    <div class="footer-column-main">
+        <h3>URUWALA</h3>
+        <h3>TEA FACTORY</h3>
+        <p style="margin-top: 20px">
+          Copyright © Uruwala Group PLC. All right reserved
+        </p>
       </div>
-      <div class="footer-column">
-        <h3>Useful Links</h3>
-        <ul>
+    <div class="footer-column">
+      <h3>Contact</h3>
+      <div class="contact-container">
+        <p>Phone: 041-2283654</p>
+        <p>Email: uruwalatea@gmail.com</p>
+      </div>
+    </div>
+    <div class="footer-column">
+      <h3>Useful Links</h3>
+      <ul>
+        <div class="col-1">
           <li><a href="#">Home</a></li>
           <li><a href="#">Product</a></li>
           <li><a href="#">Service</a></li>
+        </div>
+        <div class="col-2">
           <li><a href="#">Cart</a></li>
           <li><a href="#">About</a></li>
           <li><a href="#">Contact</a></li>
-        </ul>
-      </div>
-      <div class="footer-column">
-        <!-- Empty column for alignment -->
-      </div>
-    </footer>
-  </template>
-  
-  <script>
-  export default {
-    // Footer component logic
-  }
-  </script>
+        </div>
+      </ul>
+    </div>
+    <div class="footer-column">
+      <h3>Follow Us</h3>
+      <ul>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  // Footer component logic
+};
+</script>
 <style scoped>
 .footer-container {
-  background-color: #343a40;
+  background-color: #2a3925;
   color: #ffffff;
-  padding: 20px;
+  padding: 10px;
+  padding-bottom: 50px;
   display: flex;
   justify-content: space-between;
 }
 
 .footer-column {
   flex: 1;
+  margin-top: 5%;
 }
 
 .footer-column h3 {
@@ -65,5 +88,18 @@
 .footer-contact-details p {
   margin: 5px 0;
 }
+.contact-container {
+  margin-top: 20px;
+  text-align: center;
+}
+.col-1,
+.col-2 {
+  display: inline-block;
+  margin-right: 10px;
+  padding-top: 15px;
+}
+.footer-column-main {
+  flex: 1.5;
+  margin-top: 5%;
+}
 </style>
-  
